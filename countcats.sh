54cd6a7dbc2dc0d6334ff98fab4ea77c3cc12c84
Ben Spitz <diracdeltafunk@users.noreply.github.com>
2023-07-20 19:05:52 +0200

    Make dirs if they do not exist

--- countcats.sh
+++ countcats.sh
@@ -2,10 +2,13 @@
 
 echo "Writing minion file..."
 rm -f minion-files/category$1-$2.minion
+mkdir minion-files
 python3 generate-minion-file.py $1 $2 > minion-files/category$1-$2.minion
 echo "Running minion..."
 rm -f minion-out/out$1-$2.txt
+mkdir minion-out
 minion -findallsols -noprintsols -solsout minion-out/out$1-$2.txt minion-files/category$1-$2.minion > /dev/null 2>&1
 echo "Churning through the results..."
 rm -f database/cats$1-$2.txt
+mkdir database
 python3 process-minion-out.py $1 $2 minion-out/out$1-$2.txt database/cats$1-$2.txt
